6c7d984c5b9540aa72d4ea0602aac4ed085e0ba9
Leo Wong <leoelicos@gmail.com>
2023-03-18 21:29:18 +1100

    Prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import Home from './pages/Home'
+import Matchup from './pages/Matchup'
+import Vote from './pages/Vote'
 
 function App() {
   return (
     <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+      <div className='flex-column justify-center align-center min-100-vh bg-primary'>
         <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
+          <Route
+            path='/'
+            element={<Home />}
           />
-          <Route 
-            path="/matchup" 
-            element={<Matchup />} 
+          <Route
+            path='/matchup'
+            element={<Matchup />}
           />
-          <Route 
-            path="/matchup/:id" 
-            element={<Vote />} 
+          <Route
+            path='/matchup/:id'
+            element={<Vote />}
           />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
