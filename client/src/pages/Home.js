899e2b57de8100d6cba8f706766dff211807d26a
Leo Wong <leoelicos@gmail.com>
2023-03-18 21:30:35 +1100

    Prettier

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { getAllMatchups } from '../utils/api';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { getAllMatchups } from '../utils/api'
 
 const Home = () => {
-  const [matchupList, setMatchupList] = useState([]);
+  const [matchupList, setMatchupList] = useState([])
 
   useEffect(() => {
     const getMatchupList = async () => {
       try {
-        const res = await getAllMatchups();
+        const res = await getAllMatchups()
         if (!res.ok) {
-          throw new Error('No list of matchups');
+          throw new Error('No list of matchups')
         }
-        const matchupList = await res.json();
-        setMatchupList(matchupList);
+        const matchupList = await res.json()
+        setMatchupList(matchupList)
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
-    };
-    getMatchupList();
-  }, []);
+    }
+    getMatchupList()
+  }, [])
 
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
+    <div className='card bg-white card-rounded w-50'>
+      <div className='card-header bg-dark text-center'>
         <h1>Welcome to Tech Matchup!</h1>
       </div>
-      <div className="card-body m-5">
+      <div className='card-body m-5'>
         <h2>Here is a list of matchups you can vote on:</h2>
-        <ul className="square">
+        <ul className='square'>
           {matchupList.map((matchup) => {
             return (
               <li key={matchup._id}>
@@ -36,18 +36,18 @@ const Home = () => {
                   {matchup.tech1} vs. {matchup.tech2}
                 </Link>
               </li>
-            );
+            )
           })}
         </ul>
       </div>
-      <div className="card-footer text-center m-3">
+      <div className='card-footer text-center m-3'>
         <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
+        <Link to='/matchup'>
+          <button className='btn btn-lg btn-danger'>Create Matchup!</button>
         </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
